a012f9e07b7cb5a08f459188300476edeeaa65e4
Mustafa Munawar Anik <anik4good@gmail.com>
2021-07-09 23:10:01 +0600

    first commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+     //   $user = User::factory()->make(100);
+    }
+}
