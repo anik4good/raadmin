c4e3f84b255814fa3d4052047e891005dc11a5d8
Mustafa Munawar Anik <anik4good@gmail.com>
2021-08-02 22:31:03 +0600

    layout app info changes

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,38 +1,38 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" >
 <head>
-	<title>@yield('title','') | Radmin - Laravel Admin Starter</title>
-	<!-- initiate head with meta tags, css and script -->
-	@include('include.head')
+    <title>@yield('title','') | Radmin - Laravel Admin Starter</title>
+    <!-- initiate head with meta tags, css and script -->
+    @include('layouts.backend.partials.head')
 
 </head>
 <body id="app" >
-    <div class="wrapper">
-    	<!-- initiate header-->
-    	@include('include.header')
-    	<div class="page-wrap">
-	    	<!-- initiate sidebar-->
-	    	@include('include.sidebar')
+<div class="wrapper">
+    <!-- initiate header-->
+    @include('layouts.backend.partials.header')
+    <div class="page-wrap">
+        <!-- initiate sidebar-->
+        @include('layouts.backend.partials.sidebar')
 
-	    	<div class="main-content">
-	    		<!-- yeild contents here -->
-	    		@yield('content')
-	    	</div>
+        <div class="main-content">
+            <!-- yeild contents here -->
+            @yield('content')
+        </div>
 
-	    	<!-- initiate chat section-->
-	    	@include('include.chat')
+        <!-- initiate chat section-->
+        @include('layouts.backend.partials.chat')
 
 
-	    	<!-- initiate footer section-->
-	    	@include('include.footer')
+    <!-- initiate footer section-->
+        @include('layouts.backend.partials.footer')
 
-    	</div>
     </div>
-    
-	<!-- initiate modal menu section-->
-	@include('include.modalmenu')
+</div>
 
-	<!-- initiate scripts-->
-	@include('include.script')	
+<!-- initiate modal menu section-->
+@include('layouts.backend.partials.modalmenu')
+
+<!-- initiate scripts-->
+@include('layouts.backend.partials.script')
 </body>
 </html>
